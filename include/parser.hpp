9b144d076d2e4387c0300240f3639a166642d12a
Chanwoo Ahn <cwahn@arsvivendi.io>
2023-12-02 17:15:28 +0900

    init

---
+++ include/parser.hpp
@@ -0,0 +1,229 @@
+#ifndef EFP_PARSER_HPP_
+#define EFP_PARSER_HPP_
+
+#ifndef PARSER_HPP_
+#define PARSER_HPP_
+
+#include "prelude.hpp"
+#include "string.hpp"
+
+namespace efp
+{
+    namespace parser
+    {
+        template <typename In, typename T>
+        bool start_with(const Seq<In> &in, const Seq<T> &t)
+        {
+            const auto t_length = length(t);
+
+            if (length(in) >= t_length)
+            {
+                for (size_t i = 0; i < t_length; ++i)
+                {
+                    if (in[i] != t[i])
+                        return false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        // Ch
+
+        // template <char ch>
+        struct Ch
+        {
+            char ch;
+
+            template <typename In>
+            Maybe<Pair<StringView, StringView>> operator()(const Seq<In> &in)
+            {
+                if (length(in) > 0)
+                {
+                    if (in[0] == ch)
+                        return tuple(drop(1, in), StringView{&ch, 1});
+                    else
+                        return nothing;
+                }
+                return nothing;
+            }
+        };
+
+        // template <char c>
+        Ch ch(char c)
+        {
+            return Ch{c};
+        }
+
+        // alpha
+
+        template <typename In>
+        Maybe<Pair<StringView, StringView>> alpha0(const Seq<In> &in)
+        {
+            size_t i = 0;
+            while (i < length(in) && std::isalpha(in[i]))
+            {
+                ++i;
+            }
+
+            // Return the consumed characters and the rest of the input
+            if (i > 0)
+                return tuple(drop(i, in), take(i, in));
+            else
+                // Return the whole input and an empty string since no characters were consumed
+                return tuple(drop(0, in), StringView{in.data(), 0});
+        }
+
+        // alpha1 will parse one or more alphabetic characters
+        template <typename In>
+        Maybe<Pair<StringView, StringView>> alpha1(const Seq<In> &in)
+        {
+            size_t i = 0;
+            while (i < length(in) && std::isalpha(in[i]))
+            {
+                ++i;
+            }
+
+            // Must have consumed at least one character to succeed
+            if (i > 0)
+                return tuple(drop(i, in), take(i, in));
+            else
+                // If no characters were consumed, return nothing to indicate failure
+                return nothing;
+        }
+
+        // numeric0 will parse zero or more numeric characters
+        template <typename In>
+        Maybe<Pair<StringView, StringView>> numeric0(const Seq<In> &in)
+        {
+            size_t i = 0;
+            while (i < length(in) && std::isdigit(in[i]))
+            {
+                ++i;
+            }
+            return tuple(drop(i, in), take(i, in));
+        }
+
+        // numeric1 will parse one or more numeric characters
+        template <typename In>
+        Maybe<Pair<StringView, StringView>> numeric1(const Seq<In> &in)
+        {
+            size_t i = 0;
+            while (i < length(in) && std::isdigit(in[i]))
+            {
+                ++i;
+            }
+
+            if (i > 0)
+                return tuple(drop(i, in), take(i, in));
+            else
+                return nothing;
+        }
+
+        // alphanum0 will parse zero or more alphanumeric characters
+        template <typename In>
+        Maybe<Pair<StringView, StringView>> alphanum0(const Seq<In> &in)
+        {
+            size_t i = 0;
+            while (i < length(in) && std::isalnum(in[i]))
+            {
+                ++i;
+            }
+            return tuple(drop(i, in), take(i, in));
+        }
+
+        // alphanum1 will parse one or more alphanumeric characters
+        template <typename In>
+        Maybe<Pair<StringView, StringView>> alphanum1(const Seq<In> &in)
+        { 
+            size_t i = 0;
+            while (i < length(in) && std::isalnum(in[i]))
+            {
+                ++i;
+            }
+
+            if (i > 0)
+                return tuple(drop(i, in), take(i, in));
+            else
+                return nothing;
+        }
+
+        // Tag
+
+        struct Tag
+        {
+            StringView t;
+
+            template <typename In>
+            Maybe<Pair<StringView, StringView>> operator()(const Seq<In> &in)
+            {
+                if (start_with(in, t))
+                    return tuple(drop(length(t), in), t);
+                else
+                    return nothing;
+            }
+        };
+
+        Tag tag(const char *str)
+        {
+            return Tag{StringView{str, strlen(str)}};
+        }
+
+        // template <const char *str>
+        // struct Tag
+        // {
+        //     Maybe<Pair<StringView, StringView>> operator()(const Seq<In> &in)
+        //     {
+        //         if (start_with(in, t))
+        //             return tuple(drop(length(str), in), str);
+        //         else
+        //             return nothing;
+        //     }
+        // };
+
+        // Alt
+
+        template <typename... Ps>
+        struct Alt
+        {
+            Tuple<Ps...> ps;
+
+            template <size_t n, typename In, typename = EnableIf<(n < sizeof...(Ps))>>
+            auto impl(const Seq<In> &in) -> Maybe<Pair<StringView, StringView>>
+            {
+                const auto res = get<n>(ps)(in);
+
+                if (res)
+                    return res;
+                else
+                    return impl<n + 1>(in);
+            }
+
+            // Base case: when n equals the size of the tuple, stop recursion.
+            template <size_t n, typename In, typename = EnableIf<(n >= sizeof...(Ps))>, typename = void>
+            auto impl(const Seq<In> &in) -> Maybe<Pair<StringView, StringView>>
+            {
+                return nothing; // Or some representation of failure
+            }
+
+            template <typename In>
+            auto operator()(const Seq<In> &in) -> Maybe<Pair<StringView, StringView>>
+            {
+                return impl<0>(in);
+            }
+        };
+
+        template <typename... Ps>
+        auto alt(const Ps &...ps)
+            -> Alt<Ps...>
+        {
+            return Alt<Ps...>{tuple(ps...)};
+        }
+
+    };
+
+};
+
+#endif
+
+#endif
---
+++ test/parser_test.hpp
@@ -0,0 +1,254 @@
+#ifndef PARSER_TEST_HPP_
+#define PARSER_TEST_HPP_
+
+#include "catch2/catch_test_macros.hpp"
+
+#include "parser.hpp"
+// #include "test_common.hpp"
+
+using namespace efp;
+using namespace efp::parser;
+// using namespace efp::parser;
+
+TEST_CASE("start_with function works correctly", "[start_with]")
+{
+    SECTION("Sequence starts with the given prefix")
+    {
+        CHECK(start_with(StringView{"hello", 5}, StringView{"he", 2}));
+    }
+
+    SECTION("Sequence does not start with the given prefix")
+    {
+        CHECK_FALSE(start_with(StringView{"hello", 5}, StringView{"lo", 2}));
+    }
+}
+
+TEST_CASE("Ch parser works correctly", "[Ch]")
+{
+    // Ch c{'a'};
+
+    SECTION("Sequence starts with the character")
+    {
+        auto result = ch('a')(StringView{"apple", 5});
+
+        CHECK(result);
+        if (result)
+        {
+            CHECK(fst(result.value()).size() == 4); // "pple"
+            CHECK(snd(result.value()).size() == 1); // "a"
+        }
+    }
+
+    SECTION("Sequence does not start with the character")
+    {
+        auto result = ch('a')(StringView{"banana", 6});
+        CHECK_FALSE(result);
+    }
+}
+
+TEST_CASE("alpha0 parser works correctly", "[alpha0]")
+{
+    SECTION("Sequence with only alphabetic characters")
+    {
+        auto result = alpha0(StringView{"abcde", 5});
+        CHECK(result);
+        CHECK(fst(result.value()).size() == 0); // Remaining input is empty
+        CHECK(snd(result.value()).size() == 5); // Parsed "abcde"
+    }
+
+    SECTION("Empty sequence")
+    {
+        auto result = alpha0(StringView{"", 0});
+        CHECK(result);
+        CHECK(fst(result.value()).size() == 0); // Remaining input is empty
+        CHECK(snd(result.value()).size() == 0); // Parsed nothing
+    }
+
+    SECTION("Sequence starting with non-alphabetic characters")
+    {
+        auto result = alpha0(StringView{"123abc", 6});
+        CHECK(result);
+        CHECK(fst(result.value()).size() == 6); // Remaining input is "123abc"
+        CHECK(snd(result.value()).size() == 0); // Parsed nothing
+    }
+}
+
+TEST_CASE("alpha1 parser works correctly", "[alpha1]")
+{
+    SECTION("Sequence with only alphabetic characters")
+    {
+        auto result = alpha1(StringView{"abcde", 5});
+        CHECK(result);
+        CHECK(fst(result.value()).size() == 0); // Remaining input is empty
+        CHECK(snd(result.value()).size() == 5); // Parsed "abcde"
+    }
+
+    SECTION("Empty sequence")
+    {
+        auto result = alpha1(StringView{"", 0});
+        CHECK_FALSE(result); // Should fail as there are no characters
+    }
+
+    SECTION("Sequence starting with non-alphabetic characters")
+    {
+        auto result = alpha1(StringView{"123abc", 6});
+        CHECK_FALSE(result); // Should fail as the first character is not alphabetic
+    }
+}
+
+TEST_CASE("numeric0 parser works correctly", "[numeric0]")
+{
+    SECTION("Sequence with only numeric characters")
+    {
+        auto result = numeric0(StringView{"12345", 5});
+        CHECK(result);
+        CHECK(fst(result.value()).size() == 0); // Remaining input is empty
+        CHECK(snd(result.value()).size() == 5); // Parsed "12345"
+    }
+
+    SECTION("Empty sequence")
+    {
+        auto result = numeric0(StringView{"", 0});
+        CHECK(result);
+        CHECK(fst(result.value()).size() == 0); // Remaining input is empty
+        CHECK(snd(result.value()).size() == 0); // Parsed nothing
+    }
+
+    SECTION("Sequence starting with non-numeric characters")
+    {
+        auto result = numeric0(StringView{"abc123", 6});
+        CHECK(result);
+        CHECK(fst(result.value()).size() == 6); // Remaining input is "abc123"
+        CHECK(snd(result.value()).size() == 0); // Parsed nothing
+    }
+}
+
+TEST_CASE("numeric1 parser works correctly", "[numeric1]")
+{
+    SECTION("Sequence with only numeric characters")
+    {
+        auto result = numeric1(StringView{"12345", 5});
+        CHECK(result);
+        CHECK(fst(result.value()).size() == 0); // Remaining input is empty
+        CHECK(snd(result.value()).size() == 5); // Parsed "12345"
+    }
+
+    SECTION("Empty sequence")
+    {
+        auto result = numeric1(StringView{"", 0});
+        CHECK_FALSE(result); // Should fail as there are no characters
+    }
+
+    SECTION("Sequence starting with non-numeric characters")
+    {
+        auto result = numeric1(StringView{"abc123", 6});
+        CHECK_FALSE(result); // Should fail as the first character is not numeric
+    }
+}
+
+TEST_CASE("alphanum0 parser works correctly", "[alphanum0]")
+{
+    SECTION("Sequence with only alphanumeric characters")
+    {
+        auto result = alphanum0(StringView{"abc123", 6});
+        CHECK(result);
+        CHECK(fst(result.value()).size() == 0); // Remaining input is empty
+        CHECK(snd(result.value()).size() == 6); // Parsed "abc123"
+    }
+
+    SECTION("Empty sequence")
+    {
+        auto result = alphanum0(StringView{"", 0});
+        CHECK(result);
+        CHECK(fst(result.value()).size() == 0); // Remaining input is empty
+        CHECK(snd(result.value()).size() == 0); // Parsed nothing
+    }
+
+    SECTION("Sequence with non-alphanumeric characters")
+    {
+        auto result = alphanum0(StringView{"!@#abc123", 9});
+        CHECK(result);
+        CHECK(fst(result.value()).size() == 9); // Remaining input is "!@#abc123"
+        CHECK(snd(result.value()).size() == 0); // Parsed nothing
+    }
+}
+
+TEST_CASE("alphanum1 parser works correctly", "[alphanum1]")
+{
+    SECTION("Sequence with only alphanumeric characters")
+    {
+        auto result = alphanum1(StringView{"abc123", 6});
+        CHECK(result);
+        CHECK(fst(result.value()).size() == 0); // Remaining input is empty
+        CHECK(snd(result.value()).size() == 6); // Parsed "abc123"
+    }
+
+    SECTION("Empty sequence")
+    {
+        auto result = alphanum1(StringView{"", 0});
+        CHECK_FALSE(result); // Should fail as there are no characters
+    }
+
+    SECTION("Sequence with non-alphanumeric characters")
+    {
+        auto result = alphanum1(StringView{"!@#abc123", 9});
+        CHECK_FALSE(result); // Should fail as the first character is not alphanumeric
+    }
+}
+
+TEST_CASE("Tag parser works correctly", "[Tag]")
+{
+    // Tag tag{StringView{"<tag>", 5}};
+
+    SECTION("Sequence starts with the tag")
+    {
+        auto result = tag("<tag>")(StringView{"<tag>value", 10});
+        CHECK(result);
+        if (result)
+        {
+            CHECK(fst(result.value()).size() == 5); // "value"
+            CHECK(snd(result.value()).size() == 5); // "<tag>"
+        }
+    }
+
+    SECTION("Sequence does not start with the tag")
+    {
+        auto result = tag("<tag>")(StringView{"value<tag>", 10});
+        CHECK_FALSE(result);
+    }
+}
+
+TEST_CASE("Alt parser works correctly", "[Alt]")
+{
+    // Alt<Ch, Tag> alt{tuple(Ch{'!'}, Tag{StringView{"<tag>", 5}})};
+
+    SECTION("Alternative with character matches")
+    {
+        auto result = alt(ch('!'), tag("<tag>"))(StringView{"!value", 6});
+        CHECK(result);
+        if (result)
+        {
+            CHECK(fst(result.value()).size() == 5); // "value"
+            CHECK(snd(result.value()).size() == 1); // "!"
+        }
+    }
+
+    SECTION("Alternative with tag matches")
+    {
+        auto result = alt(ch('!'), tag("<tag>"))(StringView{"<tag>value", 10});
+        CHECK(result);
+        if (result)
+        {
+            CHECK(fst(result.value()).size() == 5); // "value"
+            CHECK(snd(result.value()).size() == 5); // "<tag>"
+        }
+    }
+
+    SECTION("No alternatives match")
+    {
+        auto result = alt(ch('!'), tag("<tag>"))(StringView{"value", 5});
+        CHECK_FALSE(result);
+    }
+}
+
+#endif
